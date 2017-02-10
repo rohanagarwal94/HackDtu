3d4234fa1ddafd242051f80509845af39f1ec696
rohanagarwal94 <rohanagarwal94@yahoo.com.sg>
2017-02-11 01:54:01 +0530

    initial commit

---
+++ client/app/src/main/java/com/example/rohanagarwal94/hackdtu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rohanagarwal94.hackdtu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
