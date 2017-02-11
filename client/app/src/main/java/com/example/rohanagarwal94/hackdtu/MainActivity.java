f9374f674afc50c1d65264f5880baa085e71bda8
rohanagarwal94 <rohanagarwal94@yahoo.com.sg>
2017-02-11 16:44:57 +0530

    Setup diabetes ui and api calls

--- client/app/src/main/java/com/example/rohanagarwal94/hackdtu/MainActivity.java
+++ client/app/src/main/java/com/example/rohanagarwal94/hackdtu/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(MainActivity.this, AllBills.class);
+                Intent i =new Intent(MainActivity.this, DiabetesActivity.class);
                 startActivity(i);
             }
         });
